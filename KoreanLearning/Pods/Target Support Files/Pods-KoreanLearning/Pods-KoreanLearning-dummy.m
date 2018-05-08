e75117fee6ce6548c45fe4957a72adc52a5e769a
Mountain Xiang <783600983@qq.com>
2018-05-08 13:34:18 +0800

    Project Initialization

---
+++ KoreanLearning/Pods/Target Support Files/Pods-KoreanLearning/Pods-KoreanLearning-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KoreanLearning : NSObject
+@end
+@implementation PodsDummy_Pods_KoreanLearning
+@end
---
+++ KoreanLearning/Pods/Target Support Files/Pods-KoreanLearningUITests/Pods-KoreanLearningUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KoreanLearningUITests : NSObject
+@end
+@implementation PodsDummy_Pods_KoreanLearningUITests
+@end
