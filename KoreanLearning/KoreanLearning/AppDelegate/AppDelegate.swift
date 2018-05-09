21cefd3de601a6c970aad7c1b7601f1791deefdf
Mountain Xiang <783600983@qq.com>
2018-05-09 10:05:10 +0800

    Organize project structure

--- KoreanLearning/KoreanLearning/AppDelegate/AppDelegate.swift
+++ KoreanLearning/KoreanLearning/AppDelegate/AppDelegate.swift
@@ -17,6 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let tabbarController = MTXTabBarController()
+        tabbarController.setup()
+        
+        self.window = UIWindow.init(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = UIColor.white
+        self.window?.makeKeyAndVisible()
+        
+        self.window?.rootViewController = tabbarController
+        
         return true
     }
 
